3179eca1befb4799f2998391fcb0265ab1dc6e11
Jorence Mendoza <jorencemendoza2@gmail.com>
2025-09-14 00:17:38 -0400

    feat

--- src/components/cards/projectCard/Projectcard.jsx
+++ src/components/cards/projectCard/Projectcard.jsx
@@ -5,51 +5,68 @@ import "./Projectcard.css";
 
 function Project() {
   return (
-    <div className="flex flex-col gap-y-8 lg:gap-x-8 lg:flex-row lg:w-3/4">
-      <div className="card flex flex-row lg:flex-col gap-x-4 rounded-lg">
-        <img className="w-1/4 lg:w-full object-cover" src={picture1} alt="" />
-        <div className="flex flex-col justify-between gap-y-2 py-2 pr-4 lg:px-5 lg:py-4">
-          <h1 className="font-medium text-xs lg:text-sm">
-            TaskFlow - Task Management Application
-          </h1>
-          <p className="text-xs lg:text-sm font-light text-justify">
-            TaskFlow is a modern, user-friendly task management application
-            designed to help individuals
-          </p>
-          <span className="text-xs lg:text-sm underline decoration-1 font-medium">
-            View Project
-          </span>
+    <div className="flex flex-col lg:items-center lg:justify-center gap-y-6 lg:gap-x-8">
+      <div className="flex flex-col gap-y-6 lg:gap-x-8 lg:flex-row lg:w-3/4">
+        <div className="card flex flex-row lg:flex-col gap-x-4 rounded-lg">
+          <img
+            className="w-1/4 lg:w-full object-cover rounded-l-lg lg:rounded-l-none"
+            src={picture1}
+            alt=""
+          />
+          <div className="flex flex-col justify-between gap-y-2 py-2 pr-4 lg:px-5 lg:py-4">
+            <h1 className="font-medium text-xs lg:text-sm">
+              TaskFlow - Task Management Application
+            </h1>
+            <p className="text-xs lg:text-sm font-light text-justify">
+              TaskFlow is a modern, user-friendly task management application
+              designed to help individuals
+            </p>
+            <span className="text-xs lg:text-sm underline decoration-1 font-medium">
+              View Project
+            </span>
+          </div>
+        </div>
+        <div className="card flex flex-row lg:flex-col gap-x-4 rounded-lg">
+          <img
+            className="w-1/4 lg:w-full object-cover rounded-l-lg lg:rounded-l-none"
+            src={picture2}
+            alt=""
+          />
+          <div className="flex flex-col justify-between gap-y-2 py-2 pr-4 lg:px-5 lg:py-4">
+            <h1 className="font-medium text-xs lg:text-sm">
+              TaskFlow - Task Management Application
+            </h1>
+            <p className="text-xs lg:text-sm font-light text-justify">
+              TaskFlow is a modern, user-friendly task management application
+              designed to help individuals
+            </p>
+            <span className="text-xs lg:text-sm underline decoration-1 font-medium">
+              View Project
+            </span>
+          </div>
+        </div>
+        <div className="card flex flex-row lg:flex-col gap-x-4 rounded-lg">
+          <img
+            className="w-1/4 lg:w-full object-cover rounded-l-lg lg:rounded-l-none"
+            src={picture3}
+            alt=""
+          />
+          <div className="flex flex-col justify-between gap-y-2 py-2 pr-4 lg:px-5 lg:py-4">
+            <h1 className="font-medium text-xs lg:text-sm">
+              TaskFlow - Task Management Application
+            </h1>
+            <p className="text-xs lg:text-sm font-light text-justify">
+              TaskFlow is a modern, user-friendly task management application
+              designed to help individuals
+            </p>
+            <span className="text-xs lg:text-sm underline decoration-1 font-medium">
+              View Project
+            </span>
+          </div>
         </div>
       </div>
-      <div className="card flex flex-row lg:flex-col gap-x-4 rounded-lg">
-        <img className="w-1/4 lg:w-full object-cover" src={picture2} alt="" />
-        <div className="flex flex-col justify-between gap-y-2 py-2 pr-4 lg:px-5 lg:py-4">
-          <h1 className="font-medium text-xs lg:text-sm">
-            TaskFlow - Task Management Application
-          </h1>
-          <p className="text-xs lg:text-sm font-light text-justify">
-            TaskFlow is a modern, user-friendly task management application
-            designed to help individuals
-          </p>
-          <span className="text-xs lg:text-sm underline decoration-1 font-medium">
-            View Project
-          </span>
-        </div>
-      </div>
-      <div className="card flex flex-row lg:flex-col gap-x-4 rounded-lg">
-        <img className="w-1/4 lg:w-full object-cover" src={picture3} alt="" />
-        <div className="flex flex-col justify-between gap-y-2 py-2 pr-4 lg:px-5 lg:py-4">
-          <h1 className="font-medium text-xs lg:text-sm">
-            TaskFlow - Task Management Application
-          </h1>
-          <p className="text-xs lg:text-sm font-light text-justify">
-            TaskFlow is a modern, user-friendly task management application
-            designed to help individuals
-          </p>
-          <span className="text-xs lg:text-sm underline decoration-1 font-medium">
-            View Project
-          </span>
-        </div>
+      <div className="">
+        <p className="font-medium underline decoration-1">See more</p>
       </div>
     </div>
   );
