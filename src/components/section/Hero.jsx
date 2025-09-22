8d2c95791d916fa9ff9a61d62ef39316d30f6640
Jorence Mendoza <jorencemendoza2@gmail.com>
2025-09-22 03:17:59 -0400

    updated

--- src/components/section/Hero.jsx
+++ src/components/section/Hero.jsx
@@ -6,15 +6,15 @@ function Hero() {
   return (
     <div className="flex flex-row sm:gap-x-2 md:gap-x-4 lg:gap-x-20 pt-20">
       <div className="w-4/5 flex flex-col gap-y-2">
-        <h1 className="text-3xl sm:text-3xl md:text-3xl lg:text-5xl font-bold">
+        <h1 className="text-3xl sm:text-3xl md:text-3xl lg:text-5xl font-bold ">
           Hi, I'm Jorence
         </h1>
-        <p className="lg:text-lg md:text-m">
+        <p className="lg:text-lg md:text-m pr-2 lg:pr-0">
           Web & Game Developer | Digital Artist. I love creating interactive worlds, building for the web, and bringing ideas to life.
         </p>
         <Button />
       </div>
-      <div className="flex justify-end items-center">
+      <div className="flex justify-end items-start">
         <img 
           src={mypicture} 
           alt="Jorence" 
