b656463950c76477075b1e37abfea75e62cb87ac
Jorence Mendoza <jorencemendoza2@gmail.com>
2025-09-26 02:33:12 -0400

    fixed

--- src/components/section/About.jsx
+++ src/components/section/About.jsx
@@ -1,9 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 function About({ darkMode }) {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setIsLoading(false), 1000); // Simulate loading time
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (isLoading) {
+    return (
+      <div
+        className={`flex flex-col lg:text-center gap-y-2 lg:my-23 p-10 rounded-xl backdrop-blur-lg animate-pulse ${
+          darkMode ? "bg-[#0f0f0f88]" : "bg-[#d4d4d449]"
+        }`}
+      >
+        <div className="w-24 h-6 mb-4 bg-gray-300 rounded"></div> {/* Skeleton for About title */}
+        <div className="w-4/5 h-4 mb-4 bg-gray-300 rounded"></div> {/* Skeleton for about description */}
+        <ul className="flex mt-5 space-x-5 lg:justify-center">
+          <li className="w-8 h-8 bg-gray-300 rounded-full"></li> {/* Skeleton for social icons */}
+          <li className="w-8 h-8 bg-gray-300 rounded-full"></li>
+          <li className="w-8 h-8 bg-gray-300 rounded-full"></li>
+          <li className="w-8 h-8 bg-gray-300 rounded-full"></li>
+        </ul>
+      </div>
+    );
+  }
+
   return (
     <div
-      className={`flex flex-col lg:text-center gap-y-2 lg:my-23  p-10 rounded-xl backdrop-blur-lg ${
+      className={`flex flex-col lg:text-center gap-y-2 lg:my-23 p-10 rounded-xl backdrop-blur-lg ${
         darkMode ? "bg-[#0f0f0f88]" : "bg-[#d4d4d449]"
       }`}
     >
@@ -17,12 +43,12 @@ function About({ darkMode }) {
       </p>
 
       <ul
-        class={`flex lg:justify-center mt-5 space-x-5 z-1 ${
+        className={`flex lg:justify-center mt-5 space-x-5 z-1 ${
           darkMode ? "text-gray-400" : "text-gray-600"
         }`}
       >
         <li>
-          <a class="" href="https://www.facebook.com/jorence.mendoza.2024">
+          <a className="" href="https://www.facebook.com/jorence.mendoza.2024">
             <svg
               aria-hidden="true"
               viewBox="0 0 24 24"
@@ -39,7 +65,7 @@ function About({ darkMode }) {
         </li>
         <li>
           <a
-            class=""
+            className=""
             href="https://www.instagram.com/jrnceee?igsh=M2k2N3ZrcGR2bWV0"
           >
             <svg
@@ -53,51 +79,6 @@ function About({ darkMode }) {
                 d="M12.315 2c2.43 0 2.784.013 3.808.06 1.064.049 1.791.218 2.427.465a4.902 4.902 0 011.772 1.153 4.902 4.902 0 011.153 1.772c.247.636.416 1.363.465 2.427.048 1.067.06 1.407.06 4.123v.08c0 2.643-.012 2.987-.06 4.043-.049 1.064-.218 1.791-.465 2.427a4.902 4.902 0 01-1.153 1.772 4.902 4.902 0 01-1.772 1.153c-.636.247-1.363.416-2.427.465-1.067.048-1.407.06-4.123.06h-.08c-2.643 0-2.987-.012-4.043-.06-1.064-.049-1.791-.218-2.427-.465a4.902 4.902 0 01-1.772-1.153 4.902 4.902 0 01-1.153-1.772c-.247-.636-.416-1.363-.465-2.427-.047-1.024-.06-1.379-.06-3.808v-.63c0-2.43.013-2.784.06-3.808.049-1.064.218-1.791.465-2.427a4.902 4.902 0 011.153-1.772A4.902 4.902 0 015.45 2.525c.636-.247 1.363-.416 2.427-.465C8.901 2.013 9.256 2 11.685 2h.63zm-.081 1.802h-.468c-2.456 0-2.784.011-3.807.058-.975.045-1.504.207-1.857.344-.467.182-.8.398-1.15.748-.35.35-.566.683-.748 1.15-.137.353-.3.882-.344 1.857-.047 1.023-.058 1.351-.058 3.807v.468c0 2.456.011 2.784.058 3.807.045.975.207 1.504.344 1.857.182.466.399.8.748 1.15.35.35.683.566 1.15.748.353.137.882.3 1.857.344 1.054.048 1.37.058 4.041.058h.08c2.597 0 2.917-.01 3.96-.058.976-.045 1.505-.207 1.858-.344.466-.182.8-.398 1.15-.748.35-.35.566-.683.748-1.15.137-.353.3-.882.344-1.857.048-1.055.058-1.37.058-4.041v-.08c0-2.597-.01-2.917-.058-3.96-.045-.976-.207-1.505-.344-1.858a3.097 3.097 0 00-.748-1.15 3.098 3.098 0 00-1.15-.748c-.353-.137-.882-.3-1.857-.344-1.023-.047-1.351-.058-3.807-.058zM12 6.865a5.135 5.135 0 110 10.27 5.135 5.135 0 010-10.27zm0 1.802a3.333 3.333 0 100 6.666 3.333 3.333 0 000-6.666zm5.338-3.205a1.2 1.2 0 110 2.4 1.2 1.2 0 010-2.4z"
                 fill-rule="evenodd"
               ></path>
-            </svg>
-          </a>
-        </li>
-        <li>
-          <a
-            class=""
-            href="https://x.com/jrnc_em?t=4JbYLuxEmGGQaDgyoMMROg&s=09"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="size-7 lg:size-10"
-              viewBox="0 0 16 16"
-            >
-              <path
-                fill="currentColor"
-                d="M9.294 6.928L14.357 1h-1.2L8.762 6.147L5.25 1H1.2l5.31 7.784L1.2 15h1.2l4.642-5.436L10.751 15h4.05zM7.651 8.852l-.538-.775L2.832 1.91h1.843l3.454 4.977l.538.775l4.491 6.47h-1.843z"
-              />
-            </svg>
-          </a>
-        </li>
-        <li>
-          <a class="" href="https://github.com/jorencd">
-            <svg
-              aria-hidden="true"
-              viewBox="0 0 24 24"
-              fill="currentColor"
-              className="size-7 lg:size-10"
-            >
-              <path
-                clip-rule="evenodd"
-                d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z"
-                fill-rule="evenodd"
-              ></path>
-            </svg>
-          </a>
-        </li>
-        <li>
-          <a class="">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="currentColor"
-              viewBox="0 0 24 24"
-              className="size-7 lg:size-10"
-            >
-              <path d="M19 3a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2zm-.5 15.5v-5.3a3.26 3.26 0 0 0-3.26-3.26c-.85 0-1.84.52-2.32 1.3v-1.11h-2.79v8.37h2.79v-4.93c0-.77.62-1.4 1.39-1.4a1.4 1.4 0 0 1 1.4 1.4v4.93zM6.88 8.56a1.68 1.68 0 0 0 1.68-1.68c0-.93-.75-1.69-1.68-1.69a1.69 1.69 0 0 0-1.69 1.69c0 .93.76 1.68 1.69 1.68m1.39 9.94v-8.37H5.5v8.37z" />
             </svg>
           </a>
         </li>
--- src/components/section/Hero.jsx
+++ src/components/section/Hero.jsx
@@ -1,9 +1,30 @@
+import { useState, useEffect } from "react";
 import mypicture from "../../assets/profile/mypicture.png";
 import Button from "../button/Button";
 
-
-
 function Hero({ darkMode, setDarkMode }) {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setIsLoading(false), 1000); // Simulate loading time
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (isLoading) {
+    return (
+      <div className="z-10 flex flex-row pt-15 sm:gap-x-2 md:gap-x-4 lg:gap-x-20">
+        <div className="flex flex-col w-4/5 gap-y-2 animate-pulse">
+          <div className="w-1/3 h-8 mb-2 bg-gray-300 rounded"></div> {/* Skeleton for the name */}
+          <div className="w-2/3 h-4 mb-2 bg-gray-300 rounded"></div> {/* Skeleton for the paragraph */}
+          <div className="w-24 h-10 bg-gray-300 rounded"></div> {/* Skeleton for the button */}
+        </div>
+        <div className="z-10 flex items-start justify-end animate-pulse">
+          <div className="bg-gray-300 rounded-full w-36 h-36"></div> {/* Skeleton for image */}
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div className="z-10 flex flex-row pt-15 sm:gap-x-2 md:gap-x-4 lg:gap-x-20">
       <div className="flex flex-col w-4/5 gap-y-2">
@@ -22,9 +43,8 @@ function Hero({ darkMode, setDarkMode }) {
           className="object-cover rounded-full w-36 lg:w-48 md:w-44 sm:w-40" 
         />
       </div>
-      
     </div>
-  )
+  );
 }
 
-export default Hero
+export default Hero;
